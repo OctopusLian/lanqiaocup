b903f0b5aad43ba3148e881fe34a2ea359f0e4e3
OctopusLian <zoctopus@qq.com>
2023-12-07 21:26:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 蓝桥杯比赛题库  
+
+本仓库所有试题基于`Python 3.8.0`实现。  
+
+## 链接  
+
+[蓝桥云课题库](https://www.lanqiao.cn/problems/)
+[蓝桥练习系统](https://lx.lanqiao.cn/problemsets.page)  
+[AcWing在线题库](https://www.acwing.com/problem/)  
---
+++ problems/254/254.py
@@ -0,0 +1,12 @@
+"""
+A+B 问题【OJ 示例题目】
+https://www.lanqiao.cn/problems/254/learning/?page=1&first_category_id=1&sort=problem_id&asc=1&name=A%2BB
+"""
+import os
+import sys
+
+# 请在此输入您的代码
+s=input().split(' ')
+a=int(s[0])
+b=int(s[1])
+print(a+b)
