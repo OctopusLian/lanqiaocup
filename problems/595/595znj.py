131fcf401b89a7d6e8e8984a0a4d002bd8e0e03f
seerzhang05 <seerzhang05@qq.com>
2024-02-21 13:19:38 +0800

    595 641(数出来的

--- problems/595/595znj.py
+++ problems/595/595znj.py
@@ -1,2 +1,12 @@
 #数出来的
-print(31)
+#print(80)
+
+l1 = 7 #只亮一个管
+l2 = 10  #ab, af, fg, bg, gc, ge, ed, cd, fe, bc
+l3 = 16 #1abg, 2afg, 3fab,4fgb,5gcd,6ged,7edc,8egc,9afe,10abc,11fed,12bcd,13fgc,14fge,15bgc,16bge
+l4 = 20 #1abgf,2gcde,3abcd,4afed,5afgc,6afge,7abgc,8abge,9fabe,10fabc,11fgbe,12fgbc,13gecf,14gecb,15gedf,16gedb,17gcdf,18gcdb,19edcf,20edcb
+l5 = 19 #abgfe1,abgfc2,gedcb3,gedcf4,abcde5,bcdef6,cdefa7,defab8,efabc9,fabcd10,afgce11,afgcd12,afged13,abgcd14,abgce15,abged16,fgbed17,fgbec18,fbgcd19
+l6 = 7 #abcdef1,abgfed2,abgfcd3,abgfec4,dcgefa5,dcgeba6,dcgefb7
+l7 = 1
+s = l1+l2+l3+l4+l5+l6+l7
+print(s)
