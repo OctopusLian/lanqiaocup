b5a093ee7e3f86e3f74722e9ac33e26aa4357fba
seerzhang05 <seerzhang05@qq.com>
2024-02-26 20:15:39 +0800

    砝码

--- problems/2134/2134znj.py
+++ problems/2134/2134znj.py
@@ -1,9 +1,4 @@
-"""
-https://www.lanqiao.cn/problems/2134/learning/?page=1&first_category_id=1&problem_id=2134
-蜂巢
-"""
-
-xdir = [-2, -1, 1, 2, 1, -1]  # 蜂巢中心
+xdir = [-2, -1, 1, 2, 1, -1]
 ydir = [0, 1, 1, 0, -1, -1]
 
 
@@ -19,6 +14,7 @@ x1, y1 = walk((d1 + 2) % 6, q1, x1, y1)
 x2, y2 = walk(d2, p2, 0, 0)
 x2, y2 = walk((d2 + 2) % 6, q2, x2, y2)
 dx, dy = abs(x1 - x2), abs(y1 - y2);
+
 if dx >= dy:
     print((dx + dy) // 2)  # 先横走，再斜着走
 else:
